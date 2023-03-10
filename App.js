cdc4cfdebcc21ee9e16bf145f4e8d98a98fbe321
Kenneth <kenneth.aduan10@gmail.com>
2023-03-10 15:41:28 +0800

    i

--- App.js
+++ App.js
@@ -3,5 +3,5 @@ import { NativeBaseProvider, Box } from "native-base";
 import Login from "./src/screens/Login/Login";
 import NavTabs from "./src/screens/NavigationTabs/NavTabs";
 export default function App() {
-  return  <NavTabs />;
+  return  <Login />;
 }

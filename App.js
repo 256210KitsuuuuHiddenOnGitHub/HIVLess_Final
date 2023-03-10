e5bc9ec7675ce9c8b7ed4cc3fbfa503fbaf84d65
Kenneth <kenneth.aduan10@gmail.com>
2023-03-10 15:41:47 +0800

    i

--- App.js
+++ App.js
@@ -3,5 +3,6 @@ import { NativeBaseProvider, Box } from "native-base";
 import Login from "./src/screens/Login/Login";
 import NavTabs from "./src/screens/NavigationTabs/NavTabs";
 export default function App() {
-  return  <Login />;
+  return  <NavTabs />;
 }
+  
--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -90,7 +90,7 @@ export default function Login() {
               marginBottom: 30,
             }}
           >
-            Forgot your Password?
+            Forgot your Password? 
           </Text>
         </TouchableOpacity>
         {/* LOGIN Button */}
